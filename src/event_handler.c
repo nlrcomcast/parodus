5fc81ecd9f16488aa11ac542b6fe8ef85794c2cf
Rangaiah <narapuram_lakshmirangaiah@comcast.com>
2023-10-13 12:14:57 +0530

    event handled added for issue debug

--- src/event_handler.c
+++ src/event_handler.c
@@ -19,8 +19,39 @@
 
 /* This is a stub file that will be overridden in a patch */
 
-#include "event_handler.h"
 
+#include "event_handler.h"
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "connection.h"
+#include "close_retry.h"
+static pthread_t sysevent_tid;
+static void *parodus_sysevent_handler (void *data)
+{
+    for (;;)
+    {
+ 	FILE *file;
+	if((file = fopen("/tmp/parodus_sysevent", "r")) != NULL) {       
+				    ParodusInfo("Received firewall_flush_conntrack event,Close the connection and retry again\n");		
+				    set_global_reconnect_reason("Firewall_Restart");
+				    set_global_reconnect_status(true);
+				    set_close_retry();
+                    break;
+    }
+    sleep(2);                        
+    }
+    while(1)
+    {
+	    sleep(10);
+	}	    
+}
 void EventHandler()
 {
+    ParodusInfo("Registered Conn Status Change callback function \n");
+    pthread_create(&sysevent_tid, NULL, parodus_sysevent_handler, NULL);    
 }
+
